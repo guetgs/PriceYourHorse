fedd5c67b2156e10d00ca1b16c20407138a82eff
Sandra Guetg <guetgs@gmail.com>
2015-11-18 12:32:22 -0800

    Lists of Keyword and Temperament Categories

--- code/feature_lists.py
+++ code/feature_lists.py
@@ -1,109 +1,18 @@
-Skills = ['Incentive Fund', 'Register Of Merit', 'Companion',\
-          'English Pleasure', 'Color Producer', 'Import', 'Homozygous Grey',\
-          'Show Experience', 'Endurance', 'All Around', 'Parade',\
-          'Racehorse', 'Finished', 'Field Hunter', 'Trick', 'Equitation',\
-          'Driving', 'Started Under Saddle', 'Dressage', 'Show Winner',\
-          'Trail', 'Athletic', 'Longe Line', 'Reining', 'Project',\
-          'Trail Riding', 'Racing', 'Show Hack', 'Breeding', 'Pony Club']
-
-Vocab_long = ['dressage', 'amateur', 'winner', 'level', 'trail', 'hunter',\
-         'championship', 'jumper', 'derby', 'riding', 'change', 'joy',\
-         'prix', 'champion', 'jumping', 'imported', 'king', 'old',\
-         'racehorse', 'showing', 'honest', 'lease', 'hack', 'athletic',\
-         'schooling', 'great', 'break', 'finished', 'trained',\
-         'equitation', 'professional', 'junior', 'futurity', 'sport',\
-         'breeding', 'beautiful', 'breeder', 'training', 'uncomplicated',\
-         'pleasure', 'nominated', 'excellent', 'division', 'premium', 'easy',\
-         'sale', 'saddle', 'test', 'movement', 'quality', 'gait',\
-         'experience', 'step', 'act', 'ring', 'eventing', 'performance',\
-         'favorite', 'good', 'earnings', 'ability', 'ridden', 'english',\
-         'talent', 'competitive', 'available', 'school', 'ready',\
-         'registered', 'started', 'rodeo', 'win', 'sire', 'new', 'sweet',\
-         'world', 'brave', 'grand', 'located', 'amazing', 'dam', 'shown',\
-         'need', 'jump', 'cross', 'lot', 'young', 'safe', 'won', 'flashy',\
-         'course', 'reserve', 'make', 'correct', 'information', 'stunning',\
-         'hand', 'successfully', 'lesson', 'experienced', 'lovely',\
-         'exceptional', 'western', 'successful', 'future', 'sporthorse',\
-         'prospect', 'fabulous', 'love', 'sane', 'ethic',\
-         'straight', 'class', 'placed', 'son', 'colt', 'picture', 'mover',\
-         'nice', 'home', 'big', 'reining', 'hauled', 'wonderful',\
-         'super', 'country', 'walk', 'opportunity', 'trainer', 'ribbon',\
-         'ease', 'run', 'form', 'ranch', 'beginner', 'spanish', 'halter',\
-         'included', 'bred', 'gorgeous', 'racing', 'novice', 'cattle',\
-         'potential', 'fantastic', 'heel', 'line', 'competing']
-
-Vocab = ['dressage', 'amateur', 'winner', 'level', 'trail', 'hunter',\
-         'championship', 'jumper', 'derby', 'joy',\
-         'prix', 'imported', 'old',\
-         'racehorse', 'showing', 'honest', 'hack', 'athletic',\
-         'schooling', 'great', 'break', 'finished', 'trained',\
-         'professional', 'junior', 'futurity', 'sport',\
-         'breeding', 'beautiful', 'uncomplicated',\
-         'pleasure', 'nominated', 'excellent', 'division', 'premium', 'easy',\
-         'movement', 'quality', 'gait',\
-         'experience', 'step', 'eventing', 'performance',\
-         'favorite', 'good', 'ridden', 'english',\
-         'talent', 'competitive', 'ready',\
-         'registered', 'started', 'rodeo', 'sweet',\
-         'brave', 'amazing',\
-         'cross', 'young', 'safe', 'flashy',\
-         'reserve', 'stunning',\
-         'hand', 'lesson', 'lovely',\
-         'exceptional', 'western', 'successful',\
-         'prospect', 'fabulous', 'sane',\
-         'straight', 'placed',\
-         'nice', 'home', 'big', 'reining', 'hauled', 'wonderful',\
-         'walk', 'opportunity',\
-         'form', 'ranch', 'beginner', 'halter',\
-         'cattle',\
+Vocab = ['dressage', 'amateur', 'winner', 'level', 'trail', 'hunter',
+         'championship', 'jumper', 'derby', 'joy', 'prix', 'imported', 'old',
+         'racehorse', 'showing', 'honest', 'hack', 'athletic', 'schooling',
+         'great', 'break', 'finished', 'trained', 'professional', 'junior',
+         'futurity', 'sport', 'breeding', 'beautiful', 'uncomplicated',
+         'pleasure', 'nominated', 'excellent', 'division', 'premium', 'easy',
+         'movement', 'quality', 'gait', 'experience', 'step', 'eventing',
+         'performance', 'favorite', 'good', 'ridden', 'english', 'talent',
+         'competitive', 'ready', 'registered', 'started', 'rodeo', 'sweet',
+         'brave', 'amazing', 'cross', 'young', 'safe', 'flashy', 'reserve',
+         'stunning', 'hand', 'lesson', 'lovely', 'exceptional', 'western',
+         'successful', 'prospect', 'fabulous', 'sane', 'straight', 'placed',
+         'nice', 'home', 'big', 'reining', 'hauled', 'wonderful', 'walk',
+         'opportunity', 'form', 'ranch', 'beginner', 'halter', 'cattle',
          'potential']
 
-Vocab_snowball = ['dressag', 'amateur', 'level', 'hunter', 'winner', 'jump',\
-                  'import', 'jumper', 'champion', 'movement', 'chang',\
-                  'success', 'trail', 'school', 'profession', 'talent',\
-                  'sport', 'train', 'old', 'premium', 'junior', 'score',\
-                  'joy', 'sporthors', 'western', 'grand', 'hack', 'experi',\
-                  'event', 'honest', 'compet', 'great', 'easi', 'pleasur',\
-                  'athlet', 'breed', 'futur', 'english', 'competit', 'love',\
-                  'spanish', 'win', 'beauti', 'ridden', 'warmblood', \
-                  'uncompl', 'excel', 'perform', 'championship', 'race',\
-                  'work', 'locat', 'shown', 'super', 'regist', 'bred',\
-                  'qualiti', 'stake', 'start', 'good', 'mover', 'nice',\
-                  'racehors', 'ribbon', 'readi', 'experienc', 'beginn',\
-                  'safe', 'intern', 'cross', 'brave', 'class', 'final',\
-                  'barrel', 'home', 'place', 'prospect', 'young', 'hunt',\
-                  'finish', 'amaz', 'hand', 'flashi', 'auto', 'look', 'stun',\
-                  'wonder', 'canter', 'world', 'breeder', 'impress', 'broke',\
-                  'filli', 'potenti', 'sweet', 'walk', 'gait', 'elig',\
-                  'price', 'season', 'halter', 'stand', 'trot', 'won',\
-                  'gorgeous', 'lot', 'fantast', 'rare', 'fenc', 'divis',\
-                  'line', 'state', 'incred', 'reserv', 'alway', 'correct',\
-                  'lesson', 'big', 'sane', 'eleg', 'littl', 'foal', 'produc',\
-                  'tempera', 'absolut', 'perfect', 'entir']
-
-
-
-Temperaments = ['0/10', '1/10', '2/10', '3/10', '4/10', '5/10',\
+Temperaments = ['0/10', '1/10', '2/10', '3/10', '4/10', '5/10',
                 '6/10', '7/10', '8/10', '9/10', '10/10']
-
-Breeds = ['Akhal Teke', 'Andalusian', 'Appaloosa', 'AraAppaloosa', 'Arabian',\
-          'Azteca', 'Barb', 'Bashkir Curly', 'Belgian', 'Belgian Warmblood',\
-          'Canadian', 'Chincoteague Pony', 'Cleveland Bay', 'Clydesdale',\
-          'Connemara Pony', 'Crossbred Pony', 'Curly', 'Dales Pony',\
-          'Donkey', 'Draft', 'Drum', 'Dutch Warmblood', 'Exmoor Pony',\
-          'Fell Pony', 'Fjord', 'Florida Cracker', 'Friesian', 'Gypsy Vanner',\
-          'Hackney', 'Haflinger', 'Half Arabian', 'Hanoverian',\
-          'Holsteiner', 'Hungarian', 'Iberian', 'Icelandic', 'Irish Draught',\
-          'Knabstrupper', 'Lipizzan', 'Lusitano', 'Miniature',\
-          'Missouri Fox Trotter', 'Morab', 'Morgan', 'Mule', 'Mustang',\
-          'New Forest Pony', 'Newfoundland Pony', 'Nokota', 'Oldenburg',\
-          'Paint', 'Paint Pony', 'Palomino', 'Paso Fino', 'Pinto',\
-          'Pony', 'Quarter Horse', 'Quarter Pony', 'Saddlebred',\
-          'Selle Francais', 'Shetland Pony', 'Shire', 'Spotted', 'Saddlebred',\
-          'Standardbred', 'Tennessee Walking', 'Thoroughbred',\
-          'Trakehner', 'Walkaloosa', 'Warmblood', 'Welsh Cob', 'Welsh Pony',\
-          'Westphalian', 'Other'] 
-
-Colors = ['Brindle', 'Grey', 'Piebald', 'Other']
-
-Sexes = ['Broodmare', 'Unborn Foal', 'Ridgling', 'Adult', 'Colt/Filly', 'Foal/Yearling']
