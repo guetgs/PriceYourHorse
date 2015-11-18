c3e1d17fccf6bb39277aba71da0a824342ed2431
Sandra Guetg <guetgs@gmail.com>
2015-11-18 12:37:16 -0800

    Regressor Model Class Inheriting From RandomForestRegressor With Additional Analytical Methods

--- code/predictor.py
+++ code/predictor.py
@@ -1,71 +1,39 @@
 import numpy as np
 from sklearn.ensemble import RandomForestRegressor
-from scipy.stats import gaussian_kde
-import seaborn as sns
-import matplotlib.pyplot as plt
 
-class Predictor(object):
-
-    def __init__(self, params):
-        self.model = RandomForestRegressor(**params)
-
-    def fit(self, X, y):
-        return self.model.fit(X, y)
-
-    def fit_transform(self, X, y):
-        return self.model.fit_transform(X, y)
-
-    def transform(self, X):
-        return self.model.transform(X)
-
-    def predict(self, X):
-        return self.model.predict(X)
-
-    def predictions(self, X):
-        trees = self.model.estimators_
-        preds = [tree.predict(X) for tree in trees]
-        return np.array(preds)
-
-    def predict_median(self, X):
-        preds = self.predictions(X)
-        return np.median(preds, axis=0)
-
-    def predict_max_kde(self, X):
-        preds = self.predictions(X)
-        p = []
-        for row in preds.T:
-            fig = plt.figure()
-            graph = sns.kdeplot(row.ravel(), shade=True, color='g', alpha=1)
-            x,y = graph.get_lines()[0].get_data()
-            ind = np.argmax(y)
-            price = np.round(x[ind], decimals=-1)
-            p.append(price)
-            plt.close(fig)
-            graph = None
-        return p
-
-
-    def score(self, X, y):
-        return self.model.score(X, y)
-
-    def cross_val_score(self):
-        return self.model.oob_score_
-
-    def feature_importance(self):
-        importances = self.model.feature_importances_
-        rank = np.argsort(importances)[::-1]
-        return importances, rank
 
 class RF_Predictor(RandomForestRegressor):
-    
+    '''
+    Predictor derived from RandomForestRegressor with additional
+    analytical methods.
+    '''
     def predictions(self, X):
+        '''
+        INPUT: 2D numpy array
+        OUTPUT: 2D numpy array
+
+        Returns predictions of individual estimators of the forest.
+        Output matrix is N_trees x N_samples.
+        '''
         preds = [tree.predict(X) for tree in self.estimators_]
         return np.array(preds)
 
     def cross_val_score(self):
+        '''
+        INPUT: None
+        OUTPUT: float
+
+        Returns out of bag score of the training data.
+        '''
         return self.oob_score_
 
     def feature_importance(self):
+        '''
+        INPUT: None
+        OUTPUT: 1D array, 1D array
+
+        Returns feature importance scores and their ranking.
+        '''
         importances = self.feature_importances_
         rank = np.argsort(importances)[::-1]
         return importances, rank
